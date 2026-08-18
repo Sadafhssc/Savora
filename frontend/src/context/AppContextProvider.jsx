4864085ec92fc82c6a2a0c5455629973caf17bab
Sadaf Javed <sadafhssc234a@gmail.com>
2026-08-18 16:06:48 +0500

    vercel setup

--- frontend/src/context/AppContextProvider.jsx
+++ frontend/src/context/AppContextProvider.jsx
@@ -5,6 +5,7 @@ import React, {
 } from "react";
 import axios from "axios";
 import toast from "react-hot-toast";
+axios.defaults.baseURL=import.meta.env.VITE_BASE_URL;
 
 export const AppContext = createContext();
 
