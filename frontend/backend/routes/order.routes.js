16e9695aac7db88a07a6e992f8bfd17908230141
Sadaf Javed <sadafhssc234a@gmail.com>
2026-08-18 15:55:16 +0500

    completed vercel setup and place order functionality

--- frontend/backend/routes/order.routes.js
+++
@@ -1,6 +0,0 @@
-import e from "express";
-import { getOrders, placeOrder } from "../controllers/order.controller.js";
-const orderRouter=e.Router();
-orderRouter.post("/place-order",placeOrder);
-orderRouter.post("/orders",getOrders);
-export default orderRouter;
