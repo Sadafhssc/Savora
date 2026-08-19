2c963c6a504b9c1b55dd6b31a15c5fcc7aa68c9d
Sadaf Javed <143011773+Sadafhssc@users.noreply.github.com>
2026-08-19 15:11:44 +0500

    Update AppContextProvider.jsx

--- frontend/src/context/AppContextProvider.jsx
+++ frontend/src/context/AppContextProvider.jsx
@@ -42,6 +42,21 @@ const AppContextProvider = ({ children }) => {
             );
         }
     };
+    const removeProduct = async (id) => {
+    try {
+        const response = await axios.post("/api/food/delete", { id });
+
+        await getProducts();
+
+        if (response.data.success) {
+            toast.success(response.data.message);
+        } else {
+            toast.error(response.data.message);
+        }
+    } catch (error) {
+        toast.error(error.message);
+    }
+};
 
 
     // ================= GET CART =================
@@ -224,6 +239,7 @@ const AppContextProvider = ({ children }) => {
 
     const value = {
         products,
+        removeProduct,
         cartItems,
         cartCount,
 
